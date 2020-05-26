d7694fa1dd964528491e8748b70d8fcda4e3f24e
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-26 21:02:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
-    return string if string.size == 1
-    last_char = string.slice(-1)
-    minus_char = variable.slice(0..-2)
-    last_char + reverse_each_word(minus_char)
+    string = string.chomp
+    reverse_each_word = ' '
+    (string.length - 1).downto(0) do |i|
+      reversed_string += string[i]
+    end
+    reverse_each_word
 end

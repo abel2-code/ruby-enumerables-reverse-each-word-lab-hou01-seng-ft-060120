dcd224bcc6dffca86052b38c0e744db2f73fc646
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-26 21:03:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
     string = string.chomp
-    reverse_each_word = ' '
+    reverse_each_word = ''
     (string.length - 1).downto(0) do |i|
       reversed_string += string[i]
     end
